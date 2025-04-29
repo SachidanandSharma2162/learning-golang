200f656e33cfe25fc16cef44a6f2bcaf779f6718
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-04-29 16:40:58 +0530

    lerned about printing, variables.

---
+++ Day1/01helloWorld.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello World")
+}
+
+// package main — Ye batata hai ki yeh program executable hai.
+
+// import "fmt" — Hum fmt (format) package use kar rahe hain output ke liye.
+
+// func main() — Program ka starting point.
+
+// fmt.Println — Console pe print karega.
