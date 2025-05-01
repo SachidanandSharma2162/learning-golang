af14506f9b8a9a512dee535b4f23c985b0d674d3
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-05-01 19:29:53 +0530

    maps done.

---
+++ Day4/11maps.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+
+func main(){
+
+	directions:=map[string]int{
+		"left":1,
+		"right":2,
+		"up":3,
+		"down":4,
+	}
+
+	fmt.Printf("%T\n",directions)
+	fmt.Println(directions)
+
+	// accessing values
+	first:=directions["left"]
+	fmt.Println(first)
+
+	// check if key exist
+	 direction,ok:=directions["up"]
+
+	if ok{
+		fmt.Println("Found:",direction)
+	}else{
+		fmt.Println("Not found")
+	}
+
+	// deletion in the map
+	delete(directions,"down")
+	fmt.Println(directions)
+
+	// looping in map
+	for key, value := range directions {
+		fmt.Printf("%s : %d\n",key,value)
+	}
+
+	// clear values
+	for k := range directions {
+		delete(directions, k)
+	}
+	fmt.Println(directions)
+}
