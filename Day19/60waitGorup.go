52127e160517ca7b879483e5cecbca6ad2eedf41
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-07-20 00:23:10 +0530

    Day 19  Completed.

--- Day19/60waitGorup.go
+++ Day19/60waitGorup.go
@@ -12,17 +12,17 @@ func sayHello(wg *sync.WaitGroup) {
 	}
 }
 func sayWorld(wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // Step 2 
 	for i := 0; i < 10; i++ {
 		fmt.Println("World")
 	}
 }
 func main() {
 	var wg sync.WaitGroup
-	
-	wg.Add(2)
+
+	wg.Add(2) // Step 1
 	go sayHello(&wg)
 	go sayWorld(&wg)
-	wg.Wait()
+	wg.Wait() // Step 3
 
 }
